5ba73e104a0e45296b0e62dcf80bc9e51c0315a4
Akash Kumar Verma <36815360+ultimateakash@users.noreply.github.com>
2022-08-07 18:35:02 +0530

    Integrate Google Analytics 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+    data: { title: 'Homepage' }
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+    data: { title: 'About Us' }
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent,
+    data: { title: 'Contact Us' }
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    data: { title: 'Not Found' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
